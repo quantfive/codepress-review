f391b4a6f0dc32b36a59d4a13862d422bfe445e5
Patrick Lu <lightning.lu7@gmail.com>
2025-06-09 23:41:46 -0700

    build: update dist

--- dist/src/review-service.js
+++ dist/src/review-service.js
@@ -107,6 +107,9 @@ class ReviewService {
             const fileName = (0, diff_parser_1.getFileNameFromChunk)(chunk);
             if (fileName) {
                 const shouldIgnore = minimatchers.some((matcher) => matcher.match(fileName));
+                console.log("fileName: ", fileName);
+                console.log("minimatchers: ", minimatchers);
+                console.log("shouldIgnore: ", shouldIgnore);
                 if (shouldIgnore) {
                     console.log(`Skipping review for ignored file: ${fileName}`);
                     continue;
